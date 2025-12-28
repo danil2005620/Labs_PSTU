fa33b10ef7c8b1115747cd99440504395c6b792b
Daniel <72625835+danil2005620@users.noreply.github.com>
2025-12-28 15:33:12 +0500

    Add files via upload

---
+++ sem_1/2_10/2_10.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "RU");
+    int N, firstmax, number;
+    cin >> N;
+    if (N <= 0) {
+        cout << "Последовательность пустая" << endl;
+        return 0;
+    }
+    cin >> firstmax;
+    for (int i = 2; i <= N; i++) {
+        cin >> number;
+        if (number > firstmax) { firstmax = number; }
+    }
+    cout << "Первый максимальный элемент: " << firstmax << endl;
+    return 0;
+}
