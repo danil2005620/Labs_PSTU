1c0d32d58edc5e4554abec00dfa7c1f4a856985e
Daniel <72625835+danil2005620@users.noreply.github.com>
2026-01-01 19:34:28 +0500

    Add files via upload

---
+++ sem_1/2_22/2_22.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "RU");
+	float a, n, max;
+	int i = 2;
+	int num = 1;
+	cin >> n;
+	max = sin(n + 1 / n);
+	while (i <= n) {
+		a = sin(n + i / n);
+		if (a < max) {
+			max = a;
+			num = i;
+		}
+		i++;
+	}
+	cout << "Макс. элемент: " << max << " Его номер: " << num << endl;
+	return 0;
+}
