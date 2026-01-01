12f9e3e81988fbcd70d88a5f528cf8d827bda342
Daniel <72625835+danil2005620@users.noreply.github.com>
2026-01-01 18:30:05 +0500

    Add files via upload

---
+++ sem_1/2_19/2_19.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int n, s;
+	s = 0;
+	cin >> n;
+	for (int i = 0; i <= n; i++) {
+		if (i % 3 != 0) {
+			s += i;
+		}
+		else {
+			s -= i;
+		}
+	}
+	cout << s << endl;
+	return 0;
+}
