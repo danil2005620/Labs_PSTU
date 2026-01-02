0b6700aa3025a768fc02f7b6f08482130bd42793
Daniel <72625835+danil2005620@users.noreply.github.com>
2026-01-02 16:13:46 +0500

    Add files via upload

---
+++ sem_1/eq_newtone/eq_newtone.cpp
@@ -0,0 +1,21 @@
+//f(x) = 0,25x**3 + x - 1,2502 na otr [0; 2], tochnost E = 0, 000001 
+//f'(x) = 0.75x**2 + 1
+//f''(x) = 1,5x
+#include <iostream> 
+#include <cmath> 
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "RU");
+    double x, x0;
+    x = 2;
+    x0 = 0;
+    double eps = 0.000001;
+    while (abs(x - x0) > eps)
+    {
+        x0 = x;
+        x = x0 - (((0.25 * pow(x, 3)) + x - 1.2502) / ((0.75 * pow(x, 2)) + 1));
+    }
+    cout << "Корень по Ньютона:" << x << endl;
+    return 0;
+}
+//Корень по Ньютона:1.00011
