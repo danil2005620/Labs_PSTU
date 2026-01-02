8451570dc750b23adc7f8967d8c7cc6583dd845e
Daniel <72625835+danil2005620@users.noreply.github.com>
2026-01-02 16:19:58 +0500

    Add files via upload

---
+++ sem_1/eq_iter/eq_iter.cpp
@@ -0,0 +1,22 @@
+//f(x) = 0,25x**3 + x - 1,2502 na otr [0; 2], tochnost E = 0, 000001 
+//f'(x) = 0.75x**2 + 1
+// f(a) = 1
+//f(b) = 4
+//lambda = -1/4
+#include <iostream> 
+#include <cmath> 
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "RU");
+    float x, x0;
+    double eps = 0.000001, lambda = -0.2;
+    x = 0;
+    x0 = 1;
+    while (abs(x - x0) > eps) {
+        x0 = x;
+        x = lambda * (0.25 * pow(x0, 3) + x0 - 1.2502) + x0;
+    }
+    cout << "Корень по итерации: " << x << endl;
+    return 0;
+}
+//Корень по итерации: 1.00011
