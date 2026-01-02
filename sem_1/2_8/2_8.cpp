ced346e7460e7b3116c18b110b69f9d2911f4cf7
Daniel <72625835+danil2005620@users.noreply.github.com>
2026-01-02 12:49:46 +0500

    Add files via upload

---
+++ sem_1/2_8/2_8.cpp
@@ -0,0 +1,19 @@
+#include <iostream> 
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "RU");
+    int N;
+    cin >> N;
+    if (N <= 2) {
+        cout << "¬ведите число больше 2." << endl;
+    }
+    else {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j <= i; j++) {
+                cout << "* ";
+            }
+            cout << endl;
+        }
+    }
+    return 0;
+}
